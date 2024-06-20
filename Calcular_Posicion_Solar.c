2cf1a0f4259ee38f6691e079abb5538878870cbb
Anthony-YP <anthony.j.yaguana@unl.edu.ec>
2024-06-19 23:04:51 -0500

    Update Calcular_Posicion_Solar.c

--- Calcular_Posicion_Solar.c
+++ Calcular_Posicion_Solar.c
@@ -11,7 +11,6 @@
 // Importamos las librerias necesarias para facilitarnos la programacion //
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <math.h>
 #include <time.h>
 #ifndef M_PI
